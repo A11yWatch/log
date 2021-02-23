c4eb5df4168dcfc9308ef6ddfa50f13d1c2a3ab3
Jeff Mendez <jeffmendez19@gmail.com>
2021-02-23 08:04:08 -0500

    chore(thread): add single child spawn

--- src/log.ts
+++ src/log.ts
@@ -8,13 +8,14 @@ import { fork } from "child_process";
 import { config } from "./config";
 import type { LogInput } from "./types";
 
+const child = fork(`${__dirname}/log-event`, [], { detached: true });
+
 const log = (message: string, options: LogInput): void => {
   if (!process.env.LOGGER_ENABLED) {
     const type = options?.type ?? "log";
     return console[typeof console[type] === "function" ? type : "log"](message);
   }
   try {
-    const child = fork(`${__dirname}/log-event`, [], { detached: true });
     child.send({ message, options, config });
     child.unref();
   } catch (e) {
